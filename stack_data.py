25b6c7813ed494d1a9af0d1a7f135dec51ecfadc
brycebolin <pie314271@gmail.com>
2017-11-04 15:34:40 -0700

    ‘firstddd’

--- stack_data.py
+++ stack_data.py
@@ -112,7 +112,7 @@ fits_file_name = output_directory + fname[i]
 stacked_name_asteroid = fits_file_name.replace('.fits',frame_interval+'_filter_' + filter_name + '_stacked_asteroid.fits')
 dat_head['EXPTIME'] = time_s.sum()
 dat_head['DATE-OBS'] = np.mean(dates_mjd)
-pyfits.writeto(stacked_name_stars,np.median(stack_array.astype(np.float32),axis=2),overwrite=True,header=dat_head)
+pyfits.writeto(stacked_name_asteroid,np.median(stack_array.astype(np.float32),axis=2),overwrite=True,header=dat_head)
 
 
 fits_file_name = center_directory+fname[0]#stack i frames for asteroids
@@ -172,7 +172,7 @@ fits_file_name = output_directory + fname[i]
 stacked_name_asteroid = fits_file_name.replace('.fits',frame_interval+'_filter_' + filter_name + '_stacked_asteroid.fits')
 dat_head['EXPTIME'] = time_s.sum()
 dat_head['DATE-OBS'] = np.mean(dates_mjd)
-pyfits.writeto(stacked_name_stars,np.median(stack_array.astype(np.float32),axis=2),overwrite=True,header=dat_head)
+pyfits.writeto(stacked_name_asteroid,np.median(stack_array.astype(np.float32),axis=2),overwrite=True,header=dat_head)
 
 
 fits_file_name = center_directory+fname[0]#stack i frames for asteroids
@@ -241,7 +241,7 @@ for qq in range(0,len(start_stop)):
     stacked_name_asteroid = fits_file_name.replace('.fits',frame_interval+'_filter_' + filter_name + '_stacked_asteroid.fits')
     dat_head['EXPTIME'] = time_s.sum()
     dat_head['DATE-OBS'] = np.mean(dates_mjd)
-    pyfits.writeto(stacked_name_stars,np.median(stack_array.astype(np.float32),axis=2),overwrite=True,header=dat_head)
+    pyfits.writeto(stacked_name_asteroid,np.median(stack_array.astype(np.float32),axis=2),overwrite=True,header=dat_head)
 
 
     fits_file_name = center_directory+fname[0]#stack i frames for asteroids
