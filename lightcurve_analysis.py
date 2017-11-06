c8c9fcf0817a74859ebd0bb53b01dd720e9cafc0
brycebolin <pie314271@gmail.com>
2017-11-05 22:37:34 -0800

    ‘firstddd’

--- lightcurve_analysis.py
+++ lightcurve_analysis.py
@@ -233,10 +233,45 @@ plt.xlim((1/maximum_frequency)*24,(1/minimum_frequency)*24)
 plt.show()
 plt.savefig('APO_DCT_combined_power_spectrum_2017_10_29_to_30.png')
 
+best_frequency = frequency[np.argmax(power)]*0.5 #need half frequency beacuse doubly peaked lightcurve
+t_fit = np.linspace(0, 0.5,len(DCTAPO_date_MJD))
+y_fit = LombScargle(DCTAPO_date_MJD, DCTAPO_mag, DCTAPO_mag_unc).model(t_fit, best_frequency) #need half frequency beacuse doubly peaked lightcurve
 
+
+phase = (DCTAPO_date_MJD * best_frequency) % 1
+
+fig, ax = plt.subplots(figsize=(6, 4.5))
+ax.errorbar(phase, DCTAPO_mag, DCTAPO_mag_unc, fmt='o', mew=0, capsize=0, elinewidth=1.5)
+ax.plot(phase, y_fit, color='black')
+ax.invert_yaxis()
+ax.set(xlabel='phase',
+       ylabel='magnitude',
+       title='phased data at frequency={0:.2f}'.format(best_frequency))
+
+
+#matplotlib.pyplot.close("all")
+
+rand = np.random.RandomState(42)
+t = 100 * rand.rand(100)
+dy = 0.1
+y = np.sin(2 * np.pi * t) + dy * rand.randn(100)
+
+frequency, power = LombScargle(t, y, dy).autopower(minimum_frequency=0.1,
+                                                   maximum_frequency=1.9)
 best_frequency = frequency[np.argmax(power)]
-t_fit = np.linspace(0, 1)
-y_fit = LombScargle(t, y, dy).model(t_fit, best_frequency)
+phase_fit = np.linspace(0, 1)
+y_fit = LombScargle(t, y, dy).model(t=phase_fit / best_frequency,
+                                    frequency=best_frequency)
+phase = (t * best_frequency) % 1
+
+fig, ax = plt.subplots(figsize=(6, 4.5))
+ax.errorbar(phase, y, dy, fmt='o', mew=0, capsize=0, elinewidth=1.5)
+ax.plot(phase_fit, y_fit, color='black')
+ax.invert_yaxis()
+ax.set(xlabel='phase',
+       ylabel='magnitude',
+       title='phased data at frequency={0:.2f}'.format(best_frequency))
+
 
 #combine DCT + APO + MPC LombScargle
 
