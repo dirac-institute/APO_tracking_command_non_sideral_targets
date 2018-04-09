bf5ef8ccf2a981384fb20f66c2f825012ff04ad1
brycebolin <pie314271@gmail.com>
2018-04-09 16:16:14 -0700

    adsfd

---
+++ tracking_command_solar_system_APO.py
@@ -0,0 +1,19 @@
+#Bryce Bolin
+'''
+
+Generates commands for tracking of solar system objects with APO
+
+e.g.
+
+tcc track 136.72666, 17.43467, -0.00000146, 0.00000046 Fk5=2000.0
+
+#astropy convert utc to mjd and back 
+t = Time('2010-01-01 00:00:00', scale='utc')
+t.mjd
+t = Time(55197.0, format='mjd', scale='utc')
+t.iso
+
+adfadsf
+adfadsfdsaf
+
+'''
