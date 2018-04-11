d9c143aa1fb715d48ac6d3e6ae8a74338e56e7ca
brycebolin <pie314271@gmail.com>
2018-04-10 17:15:35 -0700

    adsfd

--- tracking_command_solar_system_APO.py
+++ tracking_command_solar_system_APO.py
@@ -5,27 +5,8 @@ sys.path.insert(0, '/Users/bolin/NEO/Follow_up/APO_observing')
 from apo_observing_functions import *
 
 '''
-
-Generates commands for tracking of solar system objects with APO
-
-e.g.
-
-tcc track 136.72666, 17.43467, -0.00000146, 0.00000046 Fk5=2000.0
-
-get coords from JPL Horizons
-
-
-The BIG, IMPORTANT difference is that JPL gives dRA and dDec in arcsec/hour, whereas our software wants DEG?/SEC!!!!!!!!!!!!!!!!. That means YOU HAVE TO DIVIDE THE SPEED BY 12960000
-
-
-tcc track 136.72666, 17.43467, -0.00000146, 0.00000046 Fk5=2000.0 
-
-make out put to have timesteps
-2008-Apr-24 04:00:00 154.3043103 12.5870917 -2.51 0.58 tcc track 154.3043103, 12.5870917, -0.000000194, 0.000000047 Fk5=2000.0 /Rotation=0.0 /Rottype=Object
-
-dRA has cos dec multiplied
-dRA*cosD
-
+mjd for 2018 04 10
+58218.0
 ipython -i -- tracking_command_solar_system_APO.py -mpcorbf /Users/bolin/Thermal/asteroid_lists/MPCORB.DAT -ooloc /Users/bolin/NEO/OpenOrb/oorb-master/main/oorb -an 306 -stetss 44113.0 44114 0.002 -rot -67.5
 
 '''
@@ -63,6 +44,6 @@ t = Time(MJD, format='mjd', scale='utc')
 time_stamp = t.iso
 
 for i in range(0, len(MJD)):
-    print ' %5s %2s %5s %7.5f, %7.5f, %7.8f, %7.8f %7s %7s '%(time_stamp[i],'tcc','track',RA_deg[i], dec_deg[i], dRA_dt_cos_dec_deg_sec[i], ddec_dt_deg_per_deg_sec[i], 'Fk5=2000.0', '/Rotation='+rot_angle_str+' /Rottype=Object')
+    print ' %5s %2s %5s %7.5f, %7.5f, %7.8f, %7.8f %7s %7s '%(time_stamp[i],'tcc','track',RA_deg[i], dec_deg[i], dRA_dt_cos_dec_deg_sec[i], ddec_dt_deg_per_deg_sec[i], 'Fk5=2000.0', '/Rottype=Object /Rotation='+rot_angle_str)
 
 
